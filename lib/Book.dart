696f199840150abc54c6bbbac91f9ed437190e90
Nicole <silvanocolay@gmail.com>
2025-08-08 23:27:29 +0800

    Create Book.dart
    Add Book class

---
+++ lib/Book.dart
@@ -0,0 +1,12 @@
+import Book.dart;
+class Book.dart {
+  String author;
+  String title;
+  
+  tabudi (this.author, this.title);
+  
+  void describe() {
+    print('$author and $title');
+  }
+}
+Update main.dart:
