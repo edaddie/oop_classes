ed7dffc2fe17aee27ab1e26876005a71702d250c
Hvraey <edharvey.fajardo007@gmail.com>
2025-08-09 04:46:53 +0800

    Update and rename Book.dart to book.dart
    Unsa ning tabudi? T~T

--- lib/book.dart
+++ lib/book.dart
@@ -3,9 +3,9 @@ class Book{
   String author;
   String title;
   
-  tabudi (this.author, this.title);
+  Book (this.author, this.title);
   
   void describe() {
-    print('$author and $title');
+    print('$author, $title');
   }
 }
